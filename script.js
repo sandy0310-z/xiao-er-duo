8656bd72550ebb300ddc28bb82cc2ebd3eec1474
sandy0310-z <zhangshuman0310@gmail.com>
2025-07-23 11:35:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-
-function goToLevel(page) {
-    var clickSound = new Audio('audio/click.mp3');
-    clickSound.play();
-    setTimeout(() => {
-        window.location.href = page;
-    }, 500);
-}
+// JS Logic Placeholder
